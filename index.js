db8bcb19efc3564a4c02c491f704b69811552675
Chase <chaseit12@gmail.com>
2018-03-31 14:50:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = 'Welcome to Flatbook'
+
+let shortGreeting = 'Welcome'
+
+
