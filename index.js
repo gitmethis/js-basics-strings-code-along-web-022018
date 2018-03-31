2fa9b259ba5cf4e22739c44c8272e67329a3a0ff
Chase <chaseit12@gmail.com>
2018-03-31 14:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ let shortGreeting = 'Welcome'
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
-const shortGreeting = `Welcome ${currentUser[0]`
+const shortGreeting = `Welcome ${currentUser[0]}`
