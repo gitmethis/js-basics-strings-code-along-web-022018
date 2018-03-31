970b458a6b87a0680ce343649f63d68bd4fa41cd
Chase <chaseit12@gmail.com>
2018-03-31 14:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase()
 
-const shortGreeting = `Welcome ${currentUser}`
+const shortGreeting = `Welcome ${currentUser[0}`
